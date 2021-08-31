d67e48e2505d5469f8f4fe7bea6b0b3d41a635a8
tea-loves-jkdyd <aditi.6ti@gmail.com>
2021-08-31 22:31:26 +0530

    Update index.js

--- index.js
+++ index.js
@@ -186,7 +186,7 @@ function handleBackground(){
 
 //Enemies
 const enemyImage = new Image();
-enemyImage.src='red_fish.png';
+
 
 class Enemy{
     constructor(){
@@ -247,7 +247,7 @@ function handleEnemies(){
     enemy1.draw();
     enemy1.update();
 }
-
+enemyImage.src='red_fish.png';
 function  handleGameOver(){
     ctx.fillStyle='red';
     ctx.fillText('GAME OVER! Your score is: '+ score, 110, 250);
